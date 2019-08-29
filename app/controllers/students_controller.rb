e25f3350a3f6c95e038b0c34a54a661c58b403cc
aparkening <aaron.parkening@gmail.com>
2019-08-29 17:16:48 -0400

    Done. Display student show route.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,10 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  # Index route
+  def show
+    @student = Student.find(params[:id])
+  end
+
 end
